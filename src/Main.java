c5393216b107b54c5c0b62bc28d2193355b8857c
zydson123 <mlgplaygaming@gmail.com>
2022-09-15 08:47:59 +0200

    zbiory

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,28 @@
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
         //losowanie 6 liczb, zapiszemy je w tablicy potem w kolekcji
         //wpisywanie 6 liczb
         //sprawdzenie ile trafionych
         System.out.println("losowanie 6 liczb");
-        int tablica[] = new int[6];
-        for(int i=0; i<tablica.length; i++){
-            tablica[i] = (int)(Math.random() * 100 + 1);
+        //losowanie bez powtórzeń
+        //kolekcja = talice na sterydach
+        //kolekcje mogą mieć tylko typy złożone
+        //można dowolnie zmieniać rozmiar po zadeklarowaniu
+        //po zadeklarowaniu można usuwać i dodawać elementy
+        Set<Integer> wylosowane = new HashSet<>();
+        //zbiór zazwyczaj zawiera elementy bez powtórzeń
+        //zbiór zazwyczaj nie ma indeksowania elementów
+        while (wylosowane.size() < 6) {
+            wylosowane.add((int) (Math.random() * 100 + 1));
         }
-        for (int element : tablica){
-            System.out.print(element + " ");
+        int ktory = 0;
+        for (int element : wylosowane) {
+            ktory += 1;
+            System.out.println("Licza " + ktory + ": " + element + " ");
         }
-
-        int tablica2 [];
-        tablica2 = tablica;
-        tablica[0] = 0;
-        System.out.println();
-        System.out.println("tablica 1");
-        for (int element : tablica){
-            System.out.println(element + " ");
-        }
-        System.out.println("tablica 2");
-        for (int element : tablica2){
-            System.out.println(element + " ");
-        }
-        System.out.println("tablica 1");
-        System.out.println(tablica);
-        System.out.println("tablica 2");
-        System.out.println(tablica2);
-        int tablica3[] =tablica.clone();
-        tablica3[1] = 0;
-        System.out.println();
-        System.out.println("tablica 1");
-        for (int element : tablica){
-            System.out.print(element + " ");
-        }
-        System.out.println();
-        System.out.println("tablica 2");
-        for (int element : tablica2){
-            System.out.print(element + " ");
-        }
-        System.out.println();
-        System.out.println("tablica 3");
-        for (int element : tablica3){
-            System.out.print(element + " ");
-        }
-        System.out.println();
-        System.out.println(tablica3);
     }
 }
