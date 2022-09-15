27f714febea89dfccb78dc261d236a891103c257
zydson123 <mlgplaygaming@gmail.com>
2022-09-15 07:52:30 +0200

    wpisana

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Wpisz liczbę!");
+        Scanner wpisz = new Scanner(System.in);
+        int wpisana = wpisz.nextInt();
+        System.out.println("Wpisałeś " + wpisana + "!");
     }
 }
