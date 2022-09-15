d2a31764ebc45fe97208000bd550719cf8a1ff89
zydson123 <mlgplaygaming@gmail.com>
2022-09-15 08:21:06 +0200

    tabelka

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Wpisz liczbę!");
-        Scanner wpisz = new Scanner(System.in);
-        int wpisana = wpisz.nextInt();
-        System.out.println("Wpisałeś " + wpisana + "!");
+        //losowanie 6 liczb, zapiszemy je w tablicy potem w kolekcji
+        //wpisywanie 6 liczb
+        //sprawdzenie ile trafionych
+        System.out.println("losowanie 6 liczb");
+        int tablica[] = new int[6];
+        for(int i=0; i<tablica.length; i++){
+            tablica[i] = (int)(Math.random() * 100 + 1);
+        }
+        for (int element : tablica){
+            System.out.print(element + " ");
+        }
     }
 }
