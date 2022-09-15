637428a1186ee79c19f0fa315939fba43317d37b
zydson123 <mlgplaygaming@gmail.com>
2022-09-15 08:53:57 +0200

    void duzo

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,20 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
+    public static void losuj(int n, Set<Integer> tab) {
+        while (tab.size() < n) {
+            tab.add((int) (Math.random() * 100 + 1));
+        }
+    }
+
+    public static void wypisz(Set<Integer> tab) {
+        int ktory = 0;
+        for (int element : tab) {
+            ktory += 1;
+            System.out.println("Licza " + ktory + ": " + element + " ");
+        }
+    }
+
     public static void main(String[] args) {
         //losowanie 6 liczb, zapiszemy je w tablicy potem w kolekcji
         //wpisywanie 6 liczb
@@ -14,15 +28,9 @@ public class Main {
         //można dowolnie zmieniać rozmiar po zadeklarowaniu
         //po zadeklarowaniu można usuwać i dodawać elementy
         Set<Integer> wylosowane = new HashSet<>();
+        losuj(6, wylosowane);
+        wypisz(wylosowane);
         //zbiór zazwyczaj zawiera elementy bez powtórzeń
         //zbiór zazwyczaj nie ma indeksowania elementów
-        while (wylosowane.size() < 6) {
-            wylosowane.add((int) (Math.random() * 100 + 1));
-        }
-        int ktory = 0;
-        for (int element : wylosowane) {
-            ktory += 1;
-            System.out.println("Licza " + ktory + ": " + element + " ");
-        }
     }
 }
